0aad69f8b5538b89a007c68e299ce0c07c844b85
Liam PRADENE <lpradene@paul-f4Ar5s5.clusters.42paris.fr>
2024-04-26 01:28:19 +0200

    Ford Johnson but with a little bit more comparaison because don't keep track of exact positon of the paired element

--- m9/ex02/srcs/main.cpp
+++ m9/ex02/srcs/main.cpp
@@ -7,12 +7,6 @@
 #include <ctime>
 #include <sys/time.h>
 
-void    printVec(std::vector<size_t> &vec) {
-    for (std::vector<size_t>::iterator it = vec.begin(); it != vec.end(); ++it)
-        std::cout << *(it) << " ";
-    std::cout << "\n";
-}
-
 int stringToInt(std::string arg) {
     int num;
     std::stringstream   ss(arg);
@@ -22,6 +16,12 @@ int stringToInt(std::string arg) {
         throw std::runtime_error("Bad input");
     
     return (num);
+}
+
+void    printVec(std::vector<size_t> &vec) {
+    for (std::vector<size_t>::iterator it = vec.begin(); it != vec.end(); ++it)
+        std::cout << *(it) << " ";
+    std::cout << "\n";
 }
 
 std::vector<size_t> pairsSort(std::vector<size_t> &vec) {
@@ -78,24 +78,61 @@ std::vector<size_t> mergeSort(std::vector<size_t> &vec) {
     return (merge(v1, v2));
 }
 
-std::vector<size_t> insertionSort(std::vector<size_t> &vec) {
-    std::vector<size_t>::iterator  insertPos;
-    std::vector<size_t>::iterator  end = vec.begin();
-    size_t  value = 0;
-    size_t  index = 0;
 
-    for (size_t it = 1; it < vec.size(); it += 2) {
-        value = vec[it];
-        insertPos = std::upper_bound(vec.begin(), vec.begin() + it - 1, value);
+size_t  jacobstahl(size_t index) {
+    size_t  rs = 0;
+    size_t  v1 = 0;
+    size_t  v2 = 1;
+    
+    for (size_t i = 0; i < index; ++i) {
+        rs = v1 * 2 + v2;
 
-        vec.erase(vec.begin() + it);
-        vec.insert(insertPos, value);
-
-        end = vec.begin();
-        std::advance(end, ++index);
+        v1 = v2;
+        v2 = rs;
     }
 
-    return (vec);
+    return (rs);
+}
+
+std::vector<size_t> insertionSort(std::vector<size_t> &vec) {
+
+    std::vector<size_t>::iterator ps;
+    std::vector<size_t>::iterator end;
+    std::vector<size_t> XS;
+    std::vector<size_t> S;
+    size_t  inserted = 0;
+    size_t  index = 0;
+    size_t  min = 0;
+
+    for (std::vector<size_t>::iterator it = vec.begin(); it != vec.end(); ++it) {
+        S.push_back(*(it));
+        XS.push_back(*(++it));
+    }
+
+    for (size_t i = 1; 1; ++i) {
+        min = index;
+        index = jacobstahl(i);
+        if (index > XS.size())
+            break ;
+        
+        for (size_t k = index; k != min; ) {
+            end = S.begin() + k + inserted;
+            ps = std::upper_bound(S.begin(), end - 1, XS[--k]);
+            
+            S.insert(ps, XS[k]);
+            ++inserted;
+        }
+    }
+
+    for (size_t i = min; i < XS.size(); ++i) {        
+        end = S.begin() + i + inserted;
+        ps = std::upper_bound(S.begin(), end - 1, XS[i]);
+        
+        S.insert(ps, XS[i]);
+        ++inserted;
+    }
+
+    return (S);
 }
 
 void    fordJohnson(std::vector<size_t> &vec) {
@@ -140,10 +177,9 @@ int main(int ac, const char **av) {
         gettimeofday(&end, NULL);
 
         printVec(vec);
-
         size_t duration = ((end.tv_sec - start.tv_sec) * 1000000) + end.tv_usec - start.tv_usec;
         std:: cout << "Time to sort: " << duration << "ms\n";
-
+    
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
